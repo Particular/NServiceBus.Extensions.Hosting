e53416e1e7d1d4fd428d0b07c02d7801ddca9f97
Sean Feldman <feldman.sean@gmail.com>
2019-10-30 14:52:32 -0600

    Standards + initial code
    Co-authored-by: Indu Alagarsamy <indu.alagarsamy@gmail.com>

---
+++ src/NServiceBus.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.Extensions.DependencyInjection
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using NServiceBus;
+
+    /// <summary>
+    /// TODO: document
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// TODO: document
+        /// </summary>
+        public static IServiceCollection AddNServiceBus(this IServiceCollection services, EndpointConfiguration configuration)
+        {
+            var startableEndpoint = EndpointWithExternallyManagedContainer.Create(configuration, new ServiceCollectionAdapter(services));
+
+            services.AddSingleton(_ => startableEndpoint.MessageSession.Value);
+            services.AddSingleton<IHostedService>(serviceProvider => new NServiceBusService(startableEndpoint, serviceProvider));
+
+            return services;
+        }
+    }
+}
