1d3b7290d763373c2fce62cbe92b1a37b64a2a30
Sean Feldman <feldman.sean@gmail.com>
2019-11-05 12:57:35 -0700

    Move ServiceCollectionExtensions to a sub-namespace with a remark when to use it
    Co-authored-by: Indu Alagarsamy <indu.alagarsamy@gmail.com>

--- src/NServiceBus.Extensions.Hosting/HostBuilderExtensions.cs
+++ src/NServiceBus.Extensions.Hosting/HostBuilderExtensions.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using Microsoft.Extensions.Hosting;
+    using WebHost;
 
     /// <summary>
     /// Extension methods to configure NServiceBus for the .NET Core generic host.
--- src/NServiceBus.Extensions.Hosting/ServiceCollectionExtensions.cs
+++ src/NServiceBus.Extensions.Hosting/ServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿namespace NServiceBus
+﻿namespace NServiceBus.WebHost
 {
     using Extensions.Hosting;
     using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +12,10 @@
         /// <summary>
         /// Adds NServiceBus, IMessageSession, and related services to the IServiceCollection.
         /// </summary>
+        /// <remarks>
+        /// Use this extension method with WebHost only.
+        /// For GenericHost or ASP.NET Core version 3 or above use <see cref="HostBuilderExtensions.UseNServiceBus"/>
+        /// </remarks>
         public static IServiceCollection AddNServiceBus(this IServiceCollection services, EndpointConfiguration configuration)
         {
             var startableEndpoint = EndpointWithExternallyManagedContainer.Create(configuration, new ServiceCollectionAdapter(services));
