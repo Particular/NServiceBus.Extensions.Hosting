2e45c9e2d688b99649d4f2e8b1068038b4eb2f40
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-02-10 13:32:24 +0100

    add castle.windsor tests

---
+++ src/NServiceBus.Extensions.Hosting.CastleWindsor.AcceptanceTests/DefaultServer.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.AcceptanceTests.EndpointTemplates
+{
+    using System;
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Support;
+    using Castle.Windsor.MsDependencyInjection;
+    using Extensions.Hosting;
+
+    public class DefaultServer : ExternallyManagedContainerServer
+    {
+        public override Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration, Action<EndpointConfiguration> configurationBuilderCustomization)
+        {
+            return base.GetConfiguration(runDescriptor, endpointCustomizationConfiguration, endpointConfiguration =>
+            {
+                // property injection working OOTB
+                endpointConfiguration.UseContainer(new WindsorServiceProviderFactory());
+
+                configurationBuilderCustomization(endpointConfiguration);
+            });
+        }
+    }
+}
