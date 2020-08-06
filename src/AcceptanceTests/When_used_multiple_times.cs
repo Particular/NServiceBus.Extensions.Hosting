450233656c4ba8bca13fea1235226743c052fa2c
danielmarbach <daniel.marbach@openplace.net>
2020-08-06 14:08:06 +0200

    UseNServiceBus multiple times acceptance test

---
+++ src/AcceptanceTests/When_used_multiple_times.cs
@@ -0,0 +1,35 @@
+﻿namespace AcceptanceTests
+{
+    using System;
+    using Microsoft.Extensions.Hosting;
+    using NServiceBus;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_used_multiple_times
+    {
+        [Test]
+        public void Throws()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Host.CreateDefaultBuilder()
+                    .UseNServiceBus(hostBuilderContext =>
+                    {
+                        var endpointConfiguration = new EndpointConfiguration("NSBRepro");
+                        endpointConfiguration.SendOnly();
+                        endpointConfiguration.UseTransport<LearningTransport>();
+                        return endpointConfiguration;
+                    })
+                    .UseNServiceBus(hostBuilderContext =>
+                    {
+                        var endpointConfiguration = new EndpointConfiguration("NSBRepro");
+                        endpointConfiguration.SendOnly();
+                        endpointConfiguration.UseTransport<LearningTransport>();
+                        return endpointConfiguration;
+                    })
+                    .Build();
+            });
+        }
+    }
+}
