4584faff5457b72c8bb51053a41fe3c5b0f02969
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-09-21 16:01:04 +0200

    spike installer support

--- src/NServiceBus.Extensions.Hosting/HostBuilderExtensions.cs
+++ src/NServiceBus.Extensions.Hosting/HostBuilderExtensions.cs
@@ -2,8 +2,11 @@
 {
     using System;
     using Extensions.Hosting;
+    using Installation;
     using Logging;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
     using Microsoft.Extensions.Hosting;
     using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
 
@@ -31,16 +34,38 @@
                 ctx.Properties.Add(HostBuilderExtensionInUse, null);
 
                 var endpointConfiguration = endpointConfigurationBuilder(ctx);
-                var startableEndpoint = EndpointWithExternallyManagedContainer.Create(endpointConfiguration, serviceCollection);
 
-                serviceCollection.AddSingleton(_ => new HostAwareMessageSession(startableEndpoint.MessageSession));
-                serviceCollection.AddSingleton<IMessageSession>(serviceProvider => serviceProvider.GetService<HostAwareMessageSession>());
-                serviceCollection.AddSingleton<IHostedService>(serviceProvider => new NServiceBusHostedService(
-                    startableEndpoint,
-                    serviceProvider,
-                    serviceProvider.GetRequiredService<ILoggerFactory>(),
-                    deferredLoggerFactory,
-                    serviceProvider.GetRequiredService<HostAwareMessageSession>()));
+                if (ctx.Configuration.GetValue<string>("nservicebus") == "install")
+                {
+                    // clear out other hosted services that were registered before `UseNServiceBus`
+                    // Note that this won't affect hosted services registered afterwards
+                    serviceCollection.RemoveAll(typeof(IHostedService));
+
+                    var installer =
+                        Installer.CreateInstallerWithExternallyManagedContainer(endpointConfiguration,
+                            serviceCollection);
+                    serviceCollection.AddHostedService(serviceProvider =>
+                        new NServiceBusInstallerService(
+                            installer,
+                            serviceProvider.GetRequiredService<IHostApplicationLifetime>(),
+                            serviceProvider));
+
+                    // register a message session to allow dependencies to be resolved (e.g. in other hosted services)
+                    serviceCollection.AddSingleton<IMessageSession, InstallerMessageSession>();
+                }
+                else
+                {
+                    var startableEndpoint = EndpointWithExternallyManagedContainer.Create(endpointConfiguration, serviceCollection);
+
+                    serviceCollection.AddSingleton(_ => new HostAwareMessageSession(startableEndpoint.MessageSession));
+                    serviceCollection.AddSingleton<IMessageSession>(serviceProvider => serviceProvider.GetService<HostAwareMessageSession>());
+                    serviceCollection.AddSingleton<IHostedService>(serviceProvider => new NServiceBusHostedService(
+                        startableEndpoint,
+                        serviceProvider,
+                        serviceProvider.GetRequiredService<ILoggerFactory>(),
+                        deferredLoggerFactory,
+                        serviceProvider.GetRequiredService<HostAwareMessageSession>()));
+                }
             });
 
             return hostBuilder;
---
+++ src/NServiceBus.Extensions.Hosting/NServiceBusInstallerService.cs
@@ -0,0 +1,64 @@
+﻿namespace NServiceBus.Extensions.Hosting
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Installation;
+    using Microsoft.Extensions.Hosting;
+
+    class NServiceBusInstallerService : IHostedService
+    {
+        IServiceProvider serviceProvider;
+        IHostApplicationLifetime applicationLifetime;
+        InstallerWithExternallyManagedContainer installer;
+
+        public NServiceBusInstallerService(InstallerWithExternallyManagedContainer installer, IHostApplicationLifetime applicationLifetime, IServiceProvider serviceProvider)
+        {
+            this.installer = installer;
+            this.applicationLifetime = applicationLifetime;
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken = default)
+        {
+            await installer.Run(serviceProvider, cancellationToken).ConfigureAwait(false);
+            // immediately stop the application
+            // This will continue to execute other installers but will update the cancellation token
+            applicationLifetime.StopApplication();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+    }
+    class InstallerMessageSession : IMessageSession
+    {
+#pragma warning disable PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+        Task ThrowException() =>
+            throw new InvalidOperationException(
+                "Attempt to use the `IMessageSession` API while the endpoint runs in installation mode.");
+#pragma warning restore PS0018 // A task-returning method should have a CancellationToken parameter unless it has a parameter implementing ICancellableContext
+
+        public Task Send(object message, SendOptions sendOptions,
+            CancellationToken cancellationToken = new CancellationToken()) =>
+            ThrowException();
+
+        public Task Send<T>(Action<T> messageConstructor, SendOptions sendOptions,
+            CancellationToken cancellationToken = new CancellationToken()) =>
+            ThrowException();
+
+        public Task Publish(object message, PublishOptions publishOptions,
+            CancellationToken cancellationToken = new CancellationToken()) =>
+            ThrowException();
+
+        public Task Publish<T>(Action<T> messageConstructor, PublishOptions publishOptions,
+            CancellationToken cancellationToken = new CancellationToken()) =>
+            ThrowException();
+
+        public Task Subscribe(Type eventType, SubscribeOptions subscribeOptions,
+            CancellationToken cancellationToken = new CancellationToken()) =>
+            ThrowException();
+
+        public Task Unsubscribe(Type eventType, UnsubscribeOptions unsubscribeOptions,
+            CancellationToken cancellationToken = new CancellationToken()) =>
+            ThrowException();
+    }
+}
