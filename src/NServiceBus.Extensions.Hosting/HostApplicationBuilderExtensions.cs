a4597605e8b83dc4384407be47ddf9f11fe9a591
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2023-11-10 23:55:34 +0000

    Add UseNServiceBus to new .NET 8 IHostApplicationBuilder interface (#430)
    * Add new extension method for adding NServiceBus to the new .NET 8 Generic host builder approach
    
    Works for WebApplication.CreateBuilder and Host.CreateApplicationBuilder
    
    * Update to use Func<out> with no parameters as Caller will have access to Host builder
    
    * Add new signature to APIApprovals txt file for unit tests

---
+++ src/NServiceBus.Extensions.Hosting/HostApplicationBuilderExtensions.cs
@@ -0,0 +1,51 @@
+﻿namespace NServiceBus
+{
+    using System;
+    using Extensions.Hosting;
+    using Logging;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
+
+    /// <summary>
+    /// Extension methods to configure NServiceBus for the .NET Core generic host.
+    /// </summary>
+    public static class HostApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Configures the host to start an NServiceBus endpoint.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configureEndpoint"></param>
+        /// <returns></returns>
+        public static IHostApplicationBuilder UseNServiceBus(this IHostApplicationBuilder builder, Func<EndpointConfiguration> configureEndpoint)
+        {
+            var deferredLoggerFactory = new DeferredLoggerFactory();
+            LogManager.UseFactory(deferredLoggerFactory);
+
+            if (builder.Properties.ContainsKey(HostBuilderExtensionInUse))
+            {
+                throw new InvalidOperationException(
+                    "`UseNServiceBus` can only be used once on the same host instance because subsequent calls would override each other. For multi-endpoint hosting scenarios consult our documentation page.");
+            }
+
+            builder.Properties.Add(HostBuilderExtensionInUse, null);
+
+            var endpointConfiguration = configureEndpoint();
+            var startableEndpoint = EndpointWithExternallyManagedContainer.Create(endpointConfiguration, builder.Services);
+
+            builder.Services.AddSingleton(_ => new HostAwareMessageSession(startableEndpoint.MessageSession));
+            builder.Services.AddSingleton<IMessageSession>(serviceProvider => serviceProvider.GetRequiredService<HostAwareMessageSession>());
+            builder.Services.AddHostedService(serviceProvider => new NServiceBusHostedService(
+                startableEndpoint,
+                serviceProvider,
+                serviceProvider.GetRequiredService<ILoggerFactory>(),
+                deferredLoggerFactory,
+                serviceProvider.GetRequiredService<HostAwareMessageSession>()));
+
+            return builder;
+        }
+
+        const string HostBuilderExtensionInUse = "NServiceBus.Extension.Hosting.UseNServiceBus";
+    }
+}
